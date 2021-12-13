3a17599876a7319957cd0ea95d14ec9368c5ebcc
Victor <victorsmirnov@me.com>
2021-12-13 15:13:38 +0200

    create app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './service/http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-cors-errors';
+
+  content: any[] = [];
+
+  constructor(private http: HttpService) { }
+
+  ngOnInit() {
+    this.http.readJson().forEach(data => this.content = [data, ...this.content]);
+  }
 }
